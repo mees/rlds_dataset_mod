ffb93ce75104c69c78e612a87e30ee97b530dd2b
Oier Mees <oier.mees@gmail.com>
2024-01-09 15:57:22 -0800

    quick and dirty relabeling with GPT3.5

--- rlds_dataset_mod/mod_functions.py
+++ rlds_dataset_mod/mod_functions.py
@@ -3,6 +3,11 @@ from abc import ABC, abstractmethod
 import dlimp as dl
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import os
+from openai import OpenAI
+import re
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 
 class TfdsModFunction(ABC):
@@ -51,6 +56,85 @@ def mod_obs_features(features, obs_feature_mod_function):
         }
     )
 
+def extract_message(res):
+    """Parse message content from ChatGPT response."""
+    return res.choices[0].message.content
+
+
+def ask_chatgpt(messages):
+    """
+    Prompts ChatGPT with the list of messages.
+    Returns the response.
+    """
+    res = client.chat.completions.create(model="gpt-3.5-turbo",
+                                         messages=messages,
+                                         temperature=0)
+    res = extract_message(res)
+    return res
+
+class RelabelLanguage(TfdsModFunction):
+    paraphrase_base_prompt = "This is a command for a robot: %s. " \
+                             + "Can you paraphrase it into %d different versions? " \
+                             + "Be as diverse as possible without changing the meaning of the command. " \
+                             + "Number the results like 1. result, 2. result, etc"
+    n_variants = 10
+
+    prompt = paraphrase_base_prompt
+
+    def parse_response(response):
+        pattern = r'\d+\.\s(.+)'  # Match any text following a digit and period
+        # Find all matches in the response
+        matches = re.findall(pattern, response)
+        return matches
+
+    @classmethod
+    def mod_features(
+            cls,
+            features: tfds.features.FeaturesDict,
+    ) -> tfds.features.FeaturesDict:
+        features["steps"]["observation"]["language_instruction_relabel_0"] = tfds.features.Text()
+        features["steps"]["observation"]["language_instruction_relabel_1"] = tfds.features.Text()
+        features["steps"]["observation"]["language_instruction_relabel_2"] = tfds.features.Text()
+        features["steps"]["observation"]["language_instruction_relabel_3"] = tfds.features.Text()
+        features["steps"]["observation"]["language_instruction_relabel_4"] = tfds.features.Text()
+        features["steps"]["observation"]["language_instruction_relabel_5"] = tfds.features.Text()
+        features["steps"]["observation"]["language_instruction_relabel_6"] = tfds.features.Text()
+        features["steps"]["observation"]["language_instruction_relabel_7"] = tfds.features.Text()
+        features["steps"]["observation"]["language_instruction_relabel_8"] = tfds.features.Text()
+        features["steps"]["observation"]["language_instruction_relabel_9"] = tfds.features.Text()
+        return features
+
+    @classmethod
+    def mod_dataset(cls, ds: tf.data.Dataset) -> tf.data.Dataset:
+        def relabel_language(step):
+            #hacky way to find out which key is the language key, assuming its inside obs
+            lang_key_list = [key for key in step["observation"].keys() if "instruction" in key]
+            if len(lang_key_list) == 1:
+                lang_key = lang_key_list[0]
+            elif len(lang_key_list) > 1:
+                for key in lang_key_list:
+                    if "natural" in key:
+                        lang_key = key
+            if len(lang_key_list) == 0:
+                print("No language key found!!")
+
+            original_language = step["observation"][lang_key]
+            print('ORIGINAL:', original_language)
+            prompt = RelabelLanguage.prompt % (original_language, RelabelLanguage.n_variants)
+            messages = []
+            messages.append(dict(role="user", content=prompt))
+            res = ask_chatgpt(messages)
+
+            parsed_response = RelabelLanguage.parse_response(res)
+            print('AUGMENTED:', parsed_response)
+            for i, variant in enumerate(parsed_response):
+                step["observation"]["language_instruction_relabel_" + str(i)] = variant
+            return step
+        def episode_map_fn(episode):
+            episode["steps"] = episode["steps"].map(relabel_language)
+            return episode
+
+        return ds.map(episode_map_fn)
 
 class ResizeAndJpegEncode(TfdsModFunction):
     MAX_RES: int = 256
@@ -164,6 +248,7 @@ class FlipWristImgChannels(FlipImgChannels):
 
 
 TFDS_MOD_FUNCTIONS = {
+    "relabel_language": RelabelLanguage,
     "resize_and_jpeg_encode": ResizeAndJpegEncode,
     "filter_success": FilterSuccess,
     "flip_image_channels": FlipImgChannels,
